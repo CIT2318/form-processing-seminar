87306286f625147ad201fb48308a3cffa6aeade9
mmantle-hud <m.e.mantle@hud.ac.uk>
2017-10-03 10:38:30 +0100

    initial commit

---
+++ process.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>This is the PHP page</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+<body>
+<?php
+echo "It's working";
+?>
+</body>
+</html>
+
